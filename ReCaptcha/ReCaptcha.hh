0bd464c9bb318a017f1c030202cc3e1313f12740
Nathan Davies <hello@nathandavies.co.uk>
2016-03-25 19:10:57 +0000

    Hard tabs

--- ReCaptcha/ReCaptcha.hh
+++ ReCaptcha/ReCaptcha.hh
@@ -10,50 +10,50 @@ namespace ReCaptcha;
 
 class ReCaptcha
 {
-    private bool $check, $result;
-    private string $secret;
-    
-    /**
-     * Initiate
-     * @param string $secret App secret
-     */
-    public function __construct(string $secret): void
-    {
-        $this->secret = $secret;
-    }
-    
-    /**
-     * Is the user's input successful?
-     * @return bool
-     */
-    public function isSuccess() : bool
-    {
-        return $this->verify();
-    }
-    
-    /**
-     * Verify the ReCatpcha field
-     * @return bool
-     */
-    public function verify(): bool
-    {
-        if (!$this->check)
-        {
-            $c = curl_init("https://www.google.com/recaptcha/api/siteverify");
-            curl_setopt_array($c, array(
-                CURLOPT_POST => 1,
-                CURLOPT_POSTFIELDS => http_build_query(array(
-                    "secret" => $this->secret,
-                    "response" => $_POST["g-recaptcha-response"],
-                    "remoteip" => $_SERVER["REMOTE_ADDR"]
-                )),
-                CURLOPT_RETURNTRANSFER => true
-            ));
-            
-            $this->result = json_decode(curl_exec($c))->success;
-            $this->check = true;
-        }
-        
-        return $this->result;
-    }
+	private bool $check, $result;
+	private string $secret;
+
+	/**
+	 * Initiate
+	 * @param string $secret App secret
+	 */
+	public function __construct(string $secret): void
+	{
+		$this->secret = $secret;
+	}
+
+	/**
+	 * Is the user's input successful?
+	 * @return bool
+	 */
+	public function isSuccess() : bool
+	{
+		return $this->verify();
+	}
+
+	/**
+	 * Verify the ReCatpcha field
+	 * @return bool
+	 */
+	public function verify(): bool
+	{
+		if (!$this->check)
+		{
+			$c = curl_init("https://www.google.com/recaptcha/api/siteverify");
+			curl_setopt_array($c, array(
+				CURLOPT_POST => 1,
+				CURLOPT_POSTFIELDS => http_build_query(array(
+					"secret" => $this->secret,
+					"response" => $_POST["g-recaptcha-response"],
+					"remoteip" => $_SERVER["REMOTE_ADDR"]
+				)),
+				CURLOPT_RETURNTRANSFER => true
+			));
+
+			$this->result = json_decode(curl_exec($c))->success;
+			$this->check = true;
+		}
+
+		return $this->result;
+	}
 }
